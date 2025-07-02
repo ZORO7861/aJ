3e0c4bd9d842431d5a61c5d42e08575eed90f91f
ZORO7861 <prootaku786@gmail.com>
2025-07-02 20:16:42 +0530

    Update wishcute.py

--- VILLAIN_MUSIC/plugins/yumi/wishcute.py
+++ VILLAIN_MUSIC/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from VILLAIN_MUSIC import app 
 
-SUPPORT_CHAT = "PURVI_SUPPORT"
+SUPPORT_CHAT = "UniversalSociety"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
